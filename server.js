92832eb52f09a5c147a4528c7f451df454aef3bc
fatma <fatmaheshmat9@gmail.com>
2026-02-07 04:43:12 +0200

    Add graduation route to server.js

--- server.js
+++ server.js
@@ -15,10 +15,12 @@ app.use('/api/venues', require('./src/routes/venue.routes'));
 app.use('/api/outdoors', require('./src/routes/outdoor.routes'));
 app.use('/api/weddings', require('./src/routes/wedding.routes'));
 app.use('/api/birthdays', require('./src/routes/birthday.routes'));
+app.use('/api/graduations', require('./src/routes/graduation.routes'));
 
 app.listen(process.env.PORT || 5000  , () => {
   console.log("Server running 🚀");
 });
+
 
 
 
