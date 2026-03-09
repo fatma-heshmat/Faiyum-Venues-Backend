51d0c3a7d77e450d42598daddd3d83abd8249d10
fatma <fatmaheshmat9@gmail.com>
2026-03-09 23:35:06 +0200

    Enhance birthday schema with capacity and rating
    Added capacity and rating fields to birthday schema.

--- src/models/Birthday.js
+++ src/models/Birthday.js
@@ -5,7 +5,10 @@ const birthdaySchema = new mongoose.Schema({
   description: { type: String, required: true },
   price: { type: Number, required: true },
   location: { type: String, default: "Fayoum, Egypt" }, 
-  image: { type: String, required: true }
+  image: { type: String, required: true },
+  capacity: { type: Number, default: 0 },
+  rating: { type: Number, default: 0 }
 }, { timestamps: true });
+
 
 module.exports = mongoose.model("Birthday", birthdaySchema);
