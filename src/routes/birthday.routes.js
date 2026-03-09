7c9fa3988c7774ad9f938c493169e5ad0672a0a3
fatma <fatmaheshmat9@gmail.com>
2026-03-09 23:44:45 +0200

    Add route to get birthday details by ID

--- src/routes/birthday.routes.js
+++ src/routes/birthday.routes.js
@@ -1,9 +1,10 @@
 const router = require("express").Router();
-const { getBirthdays, createBirthday } = require("../controllers/birthday.controller");
+const { getBirthdays, createBirthday , getBirthdayDetails } = require("../controllers/birthday.controller");
 const multer = require('multer');
 const upload = multer({ dest: 'uploads/' });
 
 router.get("/", getBirthdays);
 router.post("/", upload.single('image'), createBirthday);
+router.get("/:id", getBirthdayDetails); // مسار الـ ID
 
 module.exports = router;
