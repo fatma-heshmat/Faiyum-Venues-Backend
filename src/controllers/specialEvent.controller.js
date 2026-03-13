e0e7a59d09659575237f25f283f386dffad92f76
fatma <fatmaheshmat9@gmail.com>
2026-03-13 21:26:25 +0200

    Update specialEvent.controller.js

--- src/controllers/specialEvent.controller.js
+++ src/controllers/specialEvent.controller.js
@@ -6,17 +6,26 @@ exports.getSpecialEvents = asyncHandler(async (req, res) => {
   res.status(200).json(events);
 });
 
+// جلب تفاصيل مناسبة واحدة بالـ ID
+exports.getSpecialEventDetails = asyncHandler(async (req, res) => {
+  const event = await SpecialEvent.findById(req.params.id);
+  if (!event) {
+    return res.status(404).json({ message: "المناسبة غير موجودة" });
+  }
+  res.status(200).json(event);
+});
+
 exports.createSpecialEvent = asyncHandler(async (req, res) => {
-  const { name, description, price, location, image } = req.body;
+  const { name, description, price, location, image , capacity, rating } = req.body;
   
   let finalImage = image;
   if (req.file) {
     const baseUrl = `${req.protocol}://${req.get('host')}`;
     finalImage = `${baseUrl}/uploads/${req.file.filename}`;
   }
-  if (!name || !price || !description || !location || !finalImage) {
+  if (!name || !price || !description || !location || !finalImage || !capacity  || !rating) {
     res.status(400);
-    throw new Error("برجاء إدخال كافة البيانات: الاسم، السعر، الوصف، والموقع");
+    throw new Error("Please Enter All Fields");
   }
   
   const event = await SpecialEvent.create({
@@ -24,7 +33,10 @@ exports.createSpecialEvent = asyncHandler(async (req, res) => {
     description, 
     price, 
     location: location || "Fayoum, Egypt", 
-    image: finalImage
+    image: finalImage,
+    capacity,
+    rating
   });
   res.status(201).json(event);
+
 });
