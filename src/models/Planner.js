a347935f9fd8eff84115d2f8b5dbf3db95425a94
fatma <fatmaheshmat9@gmail.com>
2026-03-14 00:15:31 +0200

    Update Planner.js

--- src/models/Planner.js
+++ src/models/Planner.js
@@ -1,12 +1,26 @@
 const mongoose = require("mongoose");
 
 const plannerSchema = new mongoose.Schema({
-  name: { type: String, required: true },
-  specialization: { type: String, required: true }, // التخصص (Wedding, Birthday, etc.)
-  rate: { type: Number }, // التقييم
-  pricePerHour: { type: Number, required: true }, // سعر الساعة
-  workingHours: { type: String, default: "Full time" }, // ساعات العمل
-  image: { type: String, required: true }
+ name: { type: String, required: true },
+  specialization: { type: String, required: true }, 
+  pricePerHour: { type: Number, required: true }, 
+  workingHours: { type: String, default: "Full time" }, 
+  image: { type: String, required: true },
+  
+  projectImages: [{ type: String }], // مصفوفة لصور التنفيذات السابقة
+  executionDate: { type: Date },     // تاريخ التنفيذ
+  executionCost: { type: Number },   // تكلفة التنفيذ
+  
+  rate: { type: Number , default: 0 },         // المتوسط الحسابي للتقييم
+  numberOfRatings: { type: Number, default: 0 }, // عدد الناس اللي قيموا
+  reviews: [
+    {
+      customerName: String,
+      comment: String,
+      userRating: Number,
+      date: { type: Date, default: Date.now }
+    }
+  ]
 }, { timestamps: true });
 
 module.exports = mongoose.model("Planner", plannerSchema);
