e6531c1a7dc177d41fd60b888372ce3b06586e4c
fatma <fatmaheshmat9@gmail.com>
2026-03-13 21:19:03 +0200

    Update graduation.controller.js

--- src/controllers/graduation.controller.js
+++ src/controllers/graduation.controller.js
@@ -6,8 +6,17 @@ exports.getGraduations = asyncHandler(async (req, res) => {
   res.status(200).json(graduations);
 });
 
+// جلب تفاصيل قاعة تخرج واحدة بالـ ID
+exports.getGraduationDetails = asyncHandler(async (req, res) => {
+  const graduation = await Graduation.findById(req.params.id);
+  if (!graduation) {
+    return res.status(404).json({ message: "قاعة التخرج غير موجودة" });
+  }
+  res.status(200).json(graduation);
+});
+
 exports.createGraduation = asyncHandler(async (req, res) => {
-  const { name, description, price, location, image } = req.body;
+  const { name, description, price, location, image , capacity, rating } = req.body;
   
   let finalImage = image;
   if (req.file) {
@@ -15,9 +24,9 @@ exports.createGraduation = asyncHandler(async (req, res) => {
     finalImage = `${baseUrl}/uploads/${req.file.filename}`;
   }
   
-  if (!name || !price || !description || !location || !finalImage) {
+  if (!name || !price || !description || !location || !finalImage , capacity, rating) {
     res.status(400);
-    throw new Error("برجاء إدخال كافة البيانات: الاسم، السعر، الوصف، والموقع");
+    throw new Error("Please Enter All Fields");
   }
   
   const graduation = await Graduation.create({
@@ -25,7 +34,10 @@ exports.createGraduation = asyncHandler(async (req, res) => {
     description, 
     price, 
     location: location || "Fayoum, Egypt", 
-    image: finalImage
+    image: finalImage,
+    capacity, 
+    rating
   });
   res.status(201).json(graduation);
+
 });
