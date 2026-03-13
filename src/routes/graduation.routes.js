d7635e0c836b406ce11f529ecab0482cd47a8b32
fatma <fatmaheshmat9@gmail.com>
2026-03-13 21:21:05 +0200

    Add route for fetching graduation details by ID
    Added route to get graduation details by ID.

--- src/routes/graduation.routes.js
+++ src/routes/graduation.routes.js
@@ -1,9 +1,12 @@
 const router = require("express").Router();
-const { getGraduations, createGraduation } = require("../controllers/graduation.controller");
+const { getGraduations, getGraduationDetails ,createGraduation } = require("../controllers/graduation.controller");
 const multer = require('multer');
 const upload = multer({ dest: 'uploads/' });
 
 router.get("/", getGraduations);
+// GET: لجلب تفاصيل قاعة واحدة بالـ ID
+router.get("/:id", getGraduationDetails);
 router.post("/", upload.single('image'), createGraduation);
+
 
 module.exports = router;
