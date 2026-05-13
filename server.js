4a1dfb0f0c99010bd33c25fd7ac19146c25b76a2
fatma <fatmaheshmat9@gmail.com>
2026-05-13 17:55:17 +0300

    Update server.js

--- server.js
+++ server.js
@@ -1,15 +1,20 @@
 const express = require("express");
+const cors = require('cors'); // (تعديل 1) استدعاء مكتبة الـ CORS
 const path = require('path'); // ناديت عليها بس، من غير npm install
 require("dotenv").config();
 const connectDB = require("./src/config/db");
+
 connectDB();
 
 const app = express(); // هنا بنشغل الإكسبريس
+
 // Middleware
+app.use(cors()); // (تعديل 2) السماح للفرونت إند إنه يكلم السيرفر من غير ما المتصفح يمنعه
 app.use(express.json());
 
 // السطر اللي ضفناه للصور (تأكدي إنه بعد تعريف app)
 app.use('/uploads', express.static(path.join(__dirname, 'uploads')));
+
 // هنا لازم تنادي على الـ Routes بتاعتك (تأكدي إنك ضفتيهم)
 app.use('/api/venues', require('./src/routes/venue.routes'));
 app.use('/api/outdoors', require('./src/routes/outdoor.routes'));
@@ -20,18 +25,10 @@ app.use('/api/special-events', require('./src/routes/specialEvent.routes'));
 app.use('/api/planners', require('./src/routes/planner.routes'));
 app.use('/api/auth', require('./src/routes/auth.routes'));
 app.use('/api', require('./src/routes/eventOptions.routes'));
+app.use('/api/chat', require('./src/routes/chat.routes'));
 
-app.listen(process.env.PORT || 5000  , () => {
-  console.log("Server running 🚀");
+// (تعديل 3) تظبيط البورت عشان Railway يقدر يغيره براحته
+const PORT = process.env.PORT || 5000;
+app.listen(PORT, () => {
+  console.log(`Server running on port ${PORT} 🚀`);
 });
-
-
-
-
-
-
-
-
-
-
-
