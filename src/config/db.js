72e44f30d0ed50b7aa17dfccd77025d454a54954
fatma <fatmaheshmat9@gmail.com>
2026-05-13 17:23:55 +0300

    Refactor MongoDB connection to use VenuesDB
    Updated MongoDB connection string to connect directly to VenuesDB and added logging for connection details.

--- src/config/db.js
+++ src/config/db.js
@@ -1,10 +1,16 @@
 const mongoose = require("mongoose");
+
 const connectDB = async () => {
   try {
-    await mongoose.connect(process.env.MONGO_URI);
-    console.log("✅ MongoDB Connected to Compass");
-  } catch (err) {
-    console.error("❌ Connection Error:", err.message);
+    // حطينا الرابط مباشرة هنا عشان نضمن إنه يقرأ VenuesDB ويجيب الـ 12 قاعة
+    const conn = await mongoose.connect("mongodb+srv://fatmaheshmat9:Fatma2026@cluster0.b2dfw1p.mongodb.net/VenuesDB?retryWrites=true&w=majority&appName=Cluster0");
+    
+    console.log(`✅ MongoDB Connected: ${conn.connection.host}`);
+    console.log(`📂 Database Name: ${conn.connection.name}`); // ده هيأكد لنا إننا دخلنا VenuesDB
+  } catch (error) {
+    console.error(`❌ Connection Error: ${error.message}`);
+    process.exit(1);
   }
 };
+
 module.exports = connectDB;
