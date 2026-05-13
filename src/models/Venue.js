4b57dd5d15bf37fd767726b0d9d76356e367fc72
fatma <fatmaheshmat9@gmail.com>
2026-05-13 18:02:16 +0300

    Update Venue.js

--- src/models/Venue.js
+++ src/models/Venue.js
@@ -14,5 +14,5 @@ const venueSchema = new mongoose.Schema({
   }
 }, { timestamps: true });
 
-module.exports = mongoose.model("Venue", venueSchema);
+module.exports = mongoose.model('Venue', venueSchema, 'venues');
 
