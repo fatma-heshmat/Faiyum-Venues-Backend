f46f0157f8120d4bb783d8e644d37748379bd446
fatma <fatmaheshmat9@gmail.com>
2026-06-12 18:29:50 +0300

    Simplify booking routes by removing middleware
    Removed authentication middleware from booking routes.

--- src/routes/bookingRoutes.js
+++ src/routes/bookingRoutes.js
@@ -2,16 +2,11 @@ const express = require('express');
 const router = express.Router();
 const { createBooking, getAllBookings, updateBookingStatus } = require('../controllers/bookingController');
 
-// استدعي الـ Middleware بتاع الحماية اللي إنتي كاتباه في المشروع (تأكدي من مساره)
-const { protect, admin } = require('../middleware/errorMiddleware'); 
-
-// الكاستمر لازم يكون عامل لوجن عشان يعمل بوست (protect)
-// والآدمن لازم يكون آدمن وعامل لوجن عشان يشوف كل الطلبات (protect, admin)
 router.route('/')
-    .post(protect, createBooking)      
-    .get(protect, admin, getAllBookings);     
+    .post(createBooking)      
+    .get(getAllBookings);     
 
 router.route('/:id')
-    .put(protect, admin, updateBookingStatus); // الآدمن بس اللي يقبل ويرفض
+    .put(updateBookingStatus); 
 
 module.exports = router;
