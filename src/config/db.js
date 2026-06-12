3a1d3061ac6a53b04de68ffe85cc35dd6bced8ef
fatma <fatmaheshmat9@gmail.com>
2026-06-12 19:52:26 +0300

    Update db.js

--- src/config/db.js
+++ src/config/db.js
@@ -4,7 +4,7 @@ const connectDB = async () => {
   try {
     const conn = await mongoose.connect(process.env.MONGODB_URI);
     
-    console.log(✅ MongoDB Connected to: ${conn.connection.name});
+ console.log(`✅ MongoDB Connected to: ${conn.connection.name}`);
   } catch (error) {
     console.error(❌ DB Connection Error: ${error.message});
     process.exit(1);
