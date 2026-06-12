b6e7c7738483690b0032f7cb9fd9f23b809f8252
fatma <fatmaheshmat9@gmail.com>
2026-06-12 19:36:49 +0300

    Update db.js

--- src/config/db.js
+++ src/config/db.js
@@ -2,8 +2,12 @@ const mongoose = require("mongoose");
 
 const connectDB = async () => {
   try {
-    const connString = process.env.MONGO_URI || process.env.DATABASE_URL;
+    const connString = process.env.MONGO_URI || process.env.DATABASE_URL || process.env.MONGO_URL;
     
+    if (!connString) {
+      throw new Error("Connection string is missing! Check your .env file or Railway variables.");
+    }
+
     await mongoose.connect(connString);
     console.log("✅ MongoDB Connected to Atlas");
   } catch (err) {
