81aafc912d82aaf2ef3472cd31c7169ff6b02566
fatma <fatmaheshmat9@gmail.com>
2026-06-13 00:48:02 +0300

    Update eventOptions.controller.js

--- src/controllers/eventOptions.controller.js
+++ src/controllers/eventOptions.controller.js
@@ -2,18 +2,17 @@ const EventOptions = require("../models/eventOptions");
 
 const createEventOptions = async (req, res) => {
 try {
-    const { eventDate } = req.body;
+    const { eventDate, place, placeType } = req.body;
 
-    // 1. Check if the date is already booked
-    const existingEvent = await EventOptions.findOne({ eventDate: eventDate });
+    // التأكد إن المكان ده مش محجوز في اليوم ده
+    const existingEvent = await EventOptions.findOne({ eventDate, place });
 
     if (existingEvent) {
       return res.status(400).json({
-        message: "This date is already booked. Please choose another date."
+        message: "This date is already booked for this venue."
       });
     }
 
-    // 2. If the date is available, save the new options
     const newOptions = new EventOptions(req.body);
     const savedData = await newOptions.save();
 
@@ -23,13 +22,14 @@ try {
     });
 
   } catch (error) {
-    // This will catch the 'Past Date' validation error from the model
-    res.status(400).json({
-      message: error.message
-    });
+    res.status(400).json({ message: error.message });
   }
 };
 
 module.exports = {
   createEventOptions
 };
+
+
+
+
