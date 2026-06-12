f1f46033f1a05cd8764e875fb7c1aa20acb479a8
fatma <fatmaheshmat9@gmail.com>
2026-06-12 19:19:59 +0300

    Update db.js

--- src/config/db.js
+++ src/config/db.js
@@ -1,7 +1,7 @@
 const mongoose = require("mongoose");
 const connectDB = async () => {
   try {
-   await mongoose.connect("mongodb+srv://fatma-heshmat9:Fatma2004@cluster0.fp6jf4g.mongodb.net/VenuesDB?retryWrites=true&w=majority");
+   await mongoose.connect("mongodb+srv://fatmaheshmat9:Fatma2026@cluster0.b2dfw1p.mongodb.net/VenuesDB?retryWrites=true&w=majority");
     console.log("✅ MongoDB Connected to Atlas");
   } catch (err) {
     console.error("❌ Connection Error:", err.message);
