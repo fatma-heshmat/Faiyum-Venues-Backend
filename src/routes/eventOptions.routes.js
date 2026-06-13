f79aff1daf5c34f768db5e22ce9fe6864a9f225d
fatma <fatmaheshmat9@gmail.com>
2026-06-14 01:02:41 +0300

    Update eventOptions.routes.js

--- src/routes/eventOptions.routes.js
+++ src/routes/eventOptions.routes.js
@@ -1,13 +1,9 @@
 const express = require("express");
 const router = express.Router();
 
-const { createEventOptions, getEventOptions } = require("../controllers/eventOptions.controller");
+const eventController = require("../controllers/eventOptions.controller");
 
-const { protect } = require("../middleware/auth"); 
-const { restrictTo } = require("../middleware/role.middleware");
-
-router.post("/event-options", createEventOptions);
-
-router.get("/event-options", protect, restrictTo("admin", "planner"), getEventOptions);
+router.post("/event-options", eventController.createEventOptions);
+router.get("/event-options", eventController.getEventOptions);
 
 module.exports = router;
