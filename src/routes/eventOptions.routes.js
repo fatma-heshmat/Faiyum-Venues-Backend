8c7055e4d5697f3f1a1e6328e06e95e7ca4875ed
fatma <fatmaheshmat9@gmail.com>
2026-06-14 23:13:50 +0300

    Add PATCH route for updating event status

--- src/routes/eventOptions.routes.js
+++ src/routes/eventOptions.routes.js
@@ -5,5 +5,5 @@ const eventController = require("../controllers/eventOptions.controller");
 
 router.post("/event-options", eventController.createEventOptions);
 router.get("/event-options", eventController.getEventOptions);
-
+router.patch("/event-options/:id/status", eventController.updateEventStatus);
 module.exports = router;
