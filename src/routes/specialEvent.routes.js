4c980c02cb4759ac9c1f0103c1f0aa8962b56c30
fatma <fatmaheshmat9@gmail.com>
2026-06-18 00:45:07 +0300

    Update specialEvent.routes.js

--- src/routes/specialEvent.routes.js
+++ src/routes/specialEvent.routes.js
@@ -1,11 +1,9 @@
 const router = require("express").Router();
 const { getSpecialEvents, getSpecialEventDetails, createSpecialEvent } = require("../controllers/specialEvent.controller");
 const multer = require('multer');
-const upload = multer({ dest: 'uploads/' }); // عشان يرفع الصور في الفولدر بتاعها
+const upload = multer({ dest: 'uploads/' });  
 
-// المسارات الخاصة بالـ Special Events
 router.get("/", getSpecialEvents);
-// اللينك: /api/special-events/:id
 router.get("/:id", getSpecialEventDetails);
 router.post("/", upload.single('image'), createSpecialEvent);
 
