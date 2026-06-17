61273f7a05c9c80ce440ec47e61ba413c84cd04d
fatma <fatmaheshmat9@gmail.com>
2026-06-18 00:37:31 +0300

    Update outdoor.controller.js

--- src/controllers/outdoor.controller.js
+++ src/controllers/outdoor.controller.js
@@ -1,7 +1,6 @@
 const Outdoor = require("../models/Outdoor");
 const asyncHandler = require("express-async-handler");
 
-// جلب كل القاعات الـ Outdoor
 exports.getOutdoors = asyncHandler(async (req, res) => {
   const outdoors = await Outdoor.find({});
   res.status(200).json(outdoors);
@@ -18,11 +17,9 @@ exports.getOutdoorDetails = asyncHandler(async (req, res) => {
   res.status(200).json(outdoor);
 });
 
-// إضافة قاعة Outdoor جديدة
 exports.createOutdoor = asyncHandler(async (req, res) => {
   const { name, description, price, location , image , capacity, rating } = req.body;
 
-  // لو فيه ملف مرفوع هيستخدمه، لو مفيش هيستخدم اللينك اللي في الـ body
   let finalImage = image;
   if (req.file) {
     const baseUrl = `${req.protocol}://${req.get('host')}`;
@@ -34,17 +31,9 @@ exports.createOutdoor = asyncHandler(async (req, res) => {
     throw new Error("برجاء إدخال كافة البيانات: الاسم، السعر، الوصف، والموقع");
   }
 
-
-  //if (!req.file) {
-    //res.status(400);
-    //throw new Error("برجاء رفع صورة القاعة");
-  //}
-
-  //const baseUrl = `${req.protocol}://${req.get('host')}`;
-  //const imageFullUrl = `${baseUrl}/uploads/${req.file.filename}`;
  const outdoor = await Outdoor.create({
     name, description, price, location,
-    capacity: capacity || 0, // تخزين السعة
+    capacity: capacity || 0, 
     rating: rating || 0,
     image: finalImage
   });
