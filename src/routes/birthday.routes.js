3193ba7c16db8e47ede3883fe26b9a1307361b1f
fatma <fatmaheshmat9@gmail.com>
2026-06-18 00:43:31 +0300

    Update birthday.routes.js

--- src/routes/birthday.routes.js
+++ src/routes/birthday.routes.js
@@ -7,13 +7,8 @@ const {
 const multer = require('multer');
 const upload = multer({ dest: 'uploads/' });
 
-// جلب كل أعياد الميلاد
 router.get("/", getBirthdays);
-
-// إضافة عيد ميلاد جديد مع صورة
 router.post("/", upload.single('image'), createBirthday);
-
-// جلب تفاصيل عيد ميلاد محدد بالـ ID
 router.get("/:id", getBirthdayDetails);
 
 module.exports = router;
