9561de2b63af230931da257f116588af28f96466
fatma <fatmaheshmat9@gmail.com>
2026-06-18 00:41:52 +0300

    Update Outdoor.js

--- src/models/Outdoor.js
+++ src/models/Outdoor.js
@@ -3,9 +3,9 @@ const mongoose = require("mongoose");
 const outdoorSchema = new mongoose.Schema({
   name: String,
   description: String,
-  price: Number, // السعر
-  image: String, // الصور بنفس طريقة الرفع
-  location: String, // اللوكيشن
+  price: Number, 
+  image: String, 
+  location: String, 
   capacity: { type: Number, default: 0 },
   rating: { type: Number, default: 0 }
 }, { timestamps: true });
