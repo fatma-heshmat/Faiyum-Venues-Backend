1978566780d8a296a0561704d99255797b9199cd
fatma <fatmaheshmat9@gmail.com>
2026-06-18 00:43:13 +0300

    Update Wedding.js

--- src/models/Wedding.js
+++ src/models/Wedding.js
@@ -4,7 +4,7 @@ const weddingSchema = new mongoose.Schema({
   name: { type: String, required: true },
   description: { type: String, required: true },
   price: { type: Number, required: true },
-  location: { type: String }, // اللوكيشن الاحتياطي اللي طلبتيه
+  location: { type: String }, 
   image: { type: String, required: true },
   capacity: { type: Number, default: 0 },
   rating: { type: Number, default: 0 }
