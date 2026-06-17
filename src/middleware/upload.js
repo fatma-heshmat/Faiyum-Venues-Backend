09ee10cba93dd7a231b09450ed04c195d14bf0e4
fatma <fatmaheshmat9@gmail.com>
2026-06-18 00:41:19 +0300

    Refactor upload middleware in upload.js
    Refactor upload middleware for clarity and consistency.

--- src/middleware/upload.js
+++ src/middleware/upload.js
@@ -1,14 +1,12 @@
-// middleware/upload.js
 const multer = require('multer');
 const path = require('path');
 
-// تحديد مكان التخزين واسم الملف
 const storage = multer.diskStorage({
   destination: function (req, file, cb) {
-    cb(null, 'uploads/'); // الصور هتتحفظ في فولدر اسمه uploads
+    cb(null, 'uploads/'); 
   },
   filename: function (req, file, cb) {
-    cb(null, Date.now() + path.extname(file.originalname)); // اسم فريد لكل صورة
+    cb(null, Date.now() + path.extname(file.originalname)); 
   }
 });
 
