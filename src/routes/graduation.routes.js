8bd9b70971106c24d735769b8c92f92ae1597932
fatma <fatmaheshmat9@gmail.com>
2026-06-18 00:44:11 +0300

    Update graduation.routes.js

--- src/routes/graduation.routes.js
+++ src/routes/graduation.routes.js
@@ -4,7 +4,6 @@ const multer = require('multer');
 const upload = multer({ dest: 'uploads/' });
 
 router.get("/", getGraduations);
-// GET: لجلب تفاصيل قاعة واحدة بالـ ID
 router.get("/:id", getGraduationDetails);
 router.post("/", upload.single('image'), createGraduation);
 
