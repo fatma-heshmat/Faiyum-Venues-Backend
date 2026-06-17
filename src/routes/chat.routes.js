6d06af06255b58e92aec9639f091dae00b375578
fatma <fatmaheshmat9@gmail.com>
2026-06-18 00:43:42 +0300

    Remove unnecessary comments in chat.routes.js

--- src/routes/chat.routes.js
+++ src/routes/chat.routes.js
@@ -2,7 +2,6 @@ const express = require('express');
 const router = express.Router();
 const chatController = require('../controllers/chat.controller');
 
-// لما الفرونت إند يبعت POST ريكويست، هيشغل الفانكشن بتاعة الشات
 router.post('/', chatController.handleChat);
 
 module.exports = router;
