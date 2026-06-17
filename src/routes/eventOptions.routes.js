ceb1fd807c7cb9e4415d8b76d40a3a17c95f5335
fatma <fatmaheshmat9@gmail.com>
2026-06-18 00:43:58 +0300

    Update eventOptions.routes.js

--- src/routes/eventOptions.routes.js
+++ src/routes/eventOptions.routes.js
@@ -3,11 +3,8 @@ const router = express.Router();
 const { createEventOptions, getEventOptions, updateEventStatus } = require("../controllers/eventOptions.controller");
 const auth = require("../middleware/auth"); 
 
-// 2️⃣ شرط الأمان السريع والصخر بناءً على الـ role اللي جوه التوكن
 const checkAllowedUsers = (req, res, next) => {
-  // بنسحب الرتبة اللي الميدلواير بتاعك فكها من التوكن وحطها في req.user
   const userRole = req.user && req.user.role; 
-
   if (userRole === "admin" || userRole === "planner") {
     return next(); 
   }
