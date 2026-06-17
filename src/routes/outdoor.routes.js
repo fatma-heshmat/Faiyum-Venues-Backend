3424311faf6c726e1fffe1d0ec0074b853da53ed
fatma <fatmaheshmat9@gmail.com>
2026-06-18 00:44:20 +0300

    Update outdoor.routes.js

--- src/routes/outdoor.routes.js
+++ src/routes/outdoor.routes.js
@@ -5,7 +5,6 @@ const upload = multer({ dest: 'uploads/' });
 
 router.get("/", getOutdoors);
 router.post("/", upload.single('image'), createOutdoor);
-// اللينك الخاص بكل قاعة بالـ ID
 router.get("/:id", getOutdoorDetails);
 
 module.exports = router;
